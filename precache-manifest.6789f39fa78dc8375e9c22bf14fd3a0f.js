434e548c76d0536784c0b4c7706a27f9bcabfe9a
mpetkevic <miroslav.petkevic@gmail.com>
2019-05-06 10:47:17 +0300

    Updates

---
+++ precache-manifest.6789f39fa78dc8375e9c22bf14fd3a0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2708d901118d45c0d15917a6ec515da",
+    "url": "/material-homework/index.html"
+  },
+  {
+    "revision": "d1ffc70536ae69d971bb",
+    "url": "/material-homework/static/css/main.b22c1288.chunk.css"
+  },
+  {
+    "revision": "6f69d503851a8f877c83",
+    "url": "/material-homework/static/js/2.31f499db.chunk.js"
+  },
+  {
+    "revision": "d1ffc70536ae69d971bb",
+    "url": "/material-homework/static/js/main.27bc5744.chunk.js"
+  },
+  {
+    "revision": "9b106a60f495e7aadd6c",
+    "url": "/material-homework/static/js/runtime~main.87d36740.js"
+  }
+]);
